e79a7b101a280fe2ba45d0fdf9482f2d685e0cb8
Diana Agapkina <agapkinadiana@users.noreply.github.com>
2020-10-06 14:16:24 +0300

    add: completed project

---
+++ MyCars/MyCars/ViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Diana Agapkina on 10/5/20.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    var context: NSManagedObjectContext!
+    var car: Car!
+    
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        
+        return df
+    }()
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl! {
+        didSet {
+            updateSegmentedControl()
+            
+            segmentedControl.selectedSegmentTintColor = .white
+            
+            let whiteTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            let blackTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            
+            UISegmentedControl.appearance().setTitleTextAttributes(whiteTitleTextAttributes, for: .normal)
+            UISegmentedControl.appearance().setTitleTextAttributes(blackTitleTextAttributes, for: .selected)
+        }
+    }
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getDataFromFile()
+    }
+    
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+        updateSegmentedControl()
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+        car.timesDriven += 1
+        car.lastStarted = Date()
+        
+        do {
+            try context.save()
+            insertDataFrom(selectedCar: car)
+        } catch {
+            print("Error saving context, \(error)")
+        }
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Rate It", message: "Rate this car, please", preferredStyle: .alert)
+        let rateAction = UIAlertAction(title: "Rate", style: .default) { (action) in
+            if let text = alertController.textFields?.first?.text {
+                self.update(rating: (text as NSString).doubleValue)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        alertController.addTextField { (textFiled) in
+            textFiled.keyboardType = .numberPad
+        }
+        
+        alertController.addAction(rateAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func update(rating: Double) {
+        car.rating = rating
+        
+        do {
+            try context.save()
+            insertDataFrom(selectedCar: car)
+        } catch {
+            let alertController = UIAlertController(title: "Wrong value", message: "Wrong input", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            
+            alertController.addAction(okAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+            print("Error saving context, \(error)")
+        }
+    }
+    
+    private func updateSegmentedControl() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
+        fetchRequest.predicate = NSPredicate(format: "mark = %@", mark!)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            car = results.first
+            insertDataFrom(selectedCar: car!)
+        } catch {
+            print("Error fetch data, \(error)")
+        }
+    }
+    
+    private func insertDataFrom(selectedCar car: Car) {
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timesDriven)"
+        lastTimeStartedLabel.text = "Last time started: \(dateFormatter.string(from: car.lastStarted!))"
+        segmentedControl.backgroundColor = car.tintColor as? UIColor
+    }
+    
+    private func getDataFromFile() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest )
+        } catch {
+            print("Error fetch data, \(error)")
+        }
+        
+        guard records == 0 else { return }
+        
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+              let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+        
+        for dictionary in dataArray {
+            let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+            let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! [String : AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["myChoice"] as! Bool
+            
+            let imageName = carDictionary["imageName"] as? String
+            let image = UIImage(named: imageName!)
+            let imageData = image?.pngData()
+            car.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+        }
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"], let green = colorDictionary["green"], let blue = colorDictionary["blue"] else {
+            return UIColor()
+        }
+        
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
+    
+}
+
